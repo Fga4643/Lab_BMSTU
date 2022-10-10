5411c51dc33a9788a2b25b86817ed5571469011b
Fga4643 <45387631+Fga4643@users.noreply.github.com>
2022-10-10 18:40:22 +0300

    Add files via upload

---
+++ Lab3/main.py
@@ -0,0 +1,25 @@
+from lab_python_fp.field import main1
+from lab_python_fp.gen_random import main2
+from lab_python_fp.unique import main3
+from lab_python_fp.sort import main4
+from lab_python_fp.print_result import main5
+from lab_python_fp.cm_timer import main6
+from lab_python_fp.process_data import main7
+
+
+def main():
+    main1()
+    main2()
+    main3()
+    print("---#4---")
+    main4()
+    print("---#5---")
+    main5()
+    print("---#6---")
+    main6()
+    print("---#7---")
+    main7()
+
+
+if __name__ == '__main__':
+    main()
