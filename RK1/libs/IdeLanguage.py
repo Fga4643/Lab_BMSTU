321796085bd54684157320d144b6050e6916a802
Fga4643 <45387631+Fga4643@users.noreply.github.com>
2022-10-31 17:01:11 +0300

    Add files via upload

---
+++ RK1/libs/IdeLanguage.py
@@ -0,0 +1,4 @@
+class IdeLanguage:
+    def __init__(self, languageID, ideID):
+        self.language_ID = languageID
+        self.IDE_ID = ideID
---
+++ RK1/libs/ProgrammingLanguage.py
@@ -0,0 +1,20 @@
+class ProgrammingLanguage:
+    def __init__(self, ID_language, name_language): 
+        self.__ID_language = ID_language 
+        self.__name = name_language
+    
+    @property 
+    def ID(self) -> int:
+        return self.__ID_language
+    
+    @ID.setter
+    def ID(self, ID_language): 
+        self.__ID_language = ID_language
+    
+    @property
+    def name(self) -> str: 
+        return self.__name
+    
+    @name.setter
+    def name(self, name_language): 
+        self.__name = name_language
